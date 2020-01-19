292c6995def0f97ad337842313ce69f824575b87
sarkis1231 <43452487+sarkis1231@users.noreply.github.com>
2020-01-19 13:15:12 +0400

    Update index.js

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -11,7 +11,7 @@ function capitalize(str) {
     let result = str[0].toUpperCase();
 
     for (let i = 1; i < str.length; i++) {
-        console.log(i);
+        
         if (str[i - 1] === ' ') {
             result += str[i].toUpperCase();
         } else {
